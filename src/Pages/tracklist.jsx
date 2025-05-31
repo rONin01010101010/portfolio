a0f7de83498d9211724f14b56b564c5eabb2be9c
Kenan odongo <jkenan72@gmail.com>
2025-05-31 17:49:27 -0400

    Deploy with css error fixed

--- src/Pages/tracklist.jsx
+++ src/Pages/tracklist.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './trackList.css';
+import './css/tracklist.css';
 // Import your own image below
 import Ippo from '../assets/Ippo.jpg';
 import logo from '../assets/GitHub logo - Copy.png';
