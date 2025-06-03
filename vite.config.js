4517e193d850c1807f86ef673585840ee2d35a37
Kenan odongo <jkenan72@gmail.com>
2025-06-03 00:42:05 -0400

    Edited the vite config file and package.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/kenan-portfolio',
   build: {
     outDir: 'dist',
     sourcemap: true, // Enable for debugging
