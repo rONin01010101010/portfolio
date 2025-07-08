803ae796c963f02a67b92265da4c965e11d9c6a4
Aratae <jkenan72@gmail.com>
2025-07-07 21:25:57 -0400

    Changed base path

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import App from "./App.jsx";
 // import {BrowserRouter} from "react-router-dom";
 // //import About from './Pages/About.jsx';
 // //import Project from './Pages/Project.jsx';
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 
 console.log('Main.jsx loaded');
 
