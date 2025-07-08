8048bd369f36c440c7fe32667c2cae8e198446ed
Aratae <jkenan72@gmail.com>
2025-07-07 21:39:55 -0400

    Changed base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'src',
+  base: '/main.jsx',
   build: {
     outDir: 'dist',
     sourcemap: true, // Enable for debugging
