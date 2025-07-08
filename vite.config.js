e2ad2946c39ea090f1302a15fa828bd2e4f0ded0
Aratae <jkenan72@gmail.com>
2025-07-07 21:35:56 -0400

    Changed base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'kenan-portfolio.onrender.com/',
+  base: 'src',
   build: {
     outDir: 'dist',
     sourcemap: true, // Enable for debugging
