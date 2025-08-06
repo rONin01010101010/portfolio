5a1d2b58c347d06b8ce13923d0c80a896a99f56c
Aratae <jkenan72@gmail.com>
2025-08-05 20:52:22 -0400

    edited home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Home.css';
 // Import your own image below
 import ProfilePhoto from '../assets/ProfilePhoto.jpg';
+
 const Home = () => {
-    const [isHovered, setIsHovered] = useState(false);
     const navigate = useNavigate();
 
     const handlePlayClick = () => {
@@ -15,35 +15,26 @@ const Home = () => {
         <div className="spotify-home">
             <div className="home-content">
                 <span className="newest-release">NEWEST RELEASE</span>
-
-                <div
-                    className="album-container"
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
-                >
-
+                <div className="album-container">
                     <img
                         src={ProfilePhoto}
                         alt="Portfolio Cover"
                         className="album-cover"
                     />
-
-                    {isHovered && (
-                        <div className="play-button-container">
-                            <button
-                                className="play-button"
-                                onClick={handlePlayClick}
-                                aria-label="Play and view portfolio"
-                            >
-                                <svg viewBox="0 0 24 24" width="24" height="24">
-                                    <polygon points="8,5 19,12 8,19" fill="currentColor"/>
-                                </svg>
-                            </button>
-                        </div>
-                    )}
+                    <div className="play-button-container-fixed">
+                        <button
+                            className="play-button-fixed"
+                            onClick={handlePlayClick}
+                            aria-label="Play and view portfolio"
+                        >
+                            <svg viewBox="0 0 24 24" width="20" height="20">
+                                <polygon points="8,5 19,12 8,19" fill="currentColor"/>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
-
                 <h1 className="portfolio-title">MY PORTFOLIO</h1>
+                <span className="play-instruction">CLICK THE PLAY BUTTON!!!</span>
             </div>
         </div>
     );
